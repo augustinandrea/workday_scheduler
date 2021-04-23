88f9707380e4dd84bb3a2f1dde564d68ab6a2be0
augustinandrea <augustin.andrea@gmail.com>
2021-04-23 03:44:39 -0700

    The initial commit to the repo. All a lot of things have been added. First, there was html lines of codes used to created the hour text boxes. In the JS file, I found a way to color the boxes based on hour in the day. The text in the text boxes are also saved into local storage.

---
+++ Develop/script.js
@@ -0,0 +1,54 @@
+// Create the current date
+var current_day = moment(new Date() ).format("dddd LL");
+$("#currentDay").append(current_day);
+
+// timeslot array for inputs
+var timeslots = ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"];
+var timeslotnum = [9,10,11,12,13,14,15,16,17];
+
+var hour_now = 12;//new Date().getHours();
+
+window.onload = function(){
+    for(i = 0; i < timeslots.length; i++) {
+        hour(i);
+    }
+}
+
+// hour function; have one function that inputs the hour slot
+// the function adds text and saves it to local storage
+
+function hour(slot) {
+    var text_id = "#text_" + timeslots[slot];
+    var button_id = "#button_" + timeslots[slot];
+    var hour_content = timeslots[slot] + "_content";
+    var hournum = timeslotnum[slot];
+
+    var input_text = document.querySelector(text_id);
+    var output = document.querySelector(text_id);
+
+    var save_button = document.querySelector(button_id);
+
+    save_button.addEventListener("click", update_event_output);
+
+    output.textContent = localStorage.getItem(hour_content);
+    input_text.value = localStorage.getItem(hour_content);
+
+    // function for clicking the save button
+    function update_event_output(){
+        localStorage.setItem(hour_content, input_text.value);
+
+        output.textContent = input_text.value;
+    }
+
+
+    //Changing the color depending .
+    if (hour_now < hournum) {
+        $(text_id).addClass("future");
+    } else if (hour_now === hournum) {
+        $(text_id).addClass("present");
+    } else { // hour_now > hournum
+        $(text_id).addClass("past");
+    }
+
+}
+
---
+++ README.md
@@ -0,0 +1,5 @@
+# Work Day Scheduler Starter Code
+
+In this code I create a scheduler that goes from 9AM to 5PM. At the top it lists the weekday, date, month, and year. At the bottom there are text boxes that can be wrote in. By pressing the save button, the text can be saved to local storage. The color of the boxes change depending on the time of day.
+
+![alt text](Work_day_scheduler.gif "Work Day Scheduler")
